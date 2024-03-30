607d77f7476552556f544002e11292bf456f68ec
Fury1414 <paul.toussaint1414@gmail.com>
2024-03-30 13:13:43 +0100

    finalisation sommaire

--- js.js
+++ js.js
@@ -1,9 +1,13 @@
-document.querySelector('.smooth-scroll').addEventListener('click', function(event) {
-    event.preventDefault(); // Empêche le comportement par défaut du lien
-    document.getElementById('radis').scrollIntoView({ // Fait défiler jusqu'à l'élément cible
-        behavior: 'smooth'
+document.querySelectorAll('.smooth-scroll').forEach(function(anchor) {
+    anchor.addEventListener('click', function(event) {
+        event.preventDefault(); // Empêche le comportement par défaut du lien
+        var targetId = this.getAttribute('href'); // Récupère l'ID de l'élément cible
+        var targetElement = document.querySelector(targetId); // Sélectionne l'élément cible
+        targetElement.scrollIntoView({ // Fait défiler jusqu'à l'élément cible
+            behavior: 'smooth'
+        });
+        setTimeout(function() { // Ajoute la classe active après le défilement
+            targetElement.classList.add('active');
+        }, 1000);
     });
-    setTimeout(function() { // Ajoute la classe active après le défilement
-        document.getElementById('radis').classList.add('active');
-    }, 1000);
 });
